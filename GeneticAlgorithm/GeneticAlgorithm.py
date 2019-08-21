58ab0f812d03f40de8fe6ef149fd1f2dc5e3cabd
releaseavalon <releaseavalon@163.com>
2019-08-21 19:02:07 +0800

    finished

--- GeneticAlgorithm/GeneticAlgorithm.py
+++ GeneticAlgorithm/GeneticAlgorithm.py
@@ -78,25 +78,65 @@ class GA():
   
   # 得到适应度
   def get_fitness(self, non_negative=False):
-    pass
+    # 用于对原始适应值做处理, 此处为得到非负的适应值
+    result = self.func(*np.array(list(zip(*self.translateDNA()))))
+    if non_negative:
+      min_fit = np.min(result, axis=0)
+      result -= (min_fit - 0.000001)
+    return result
+
   # 选择
   def select(self):
-    pass
+    fitness = self.get_fitness(True)
+    self.POP = self.POP[np.random.choice(np.array(self.POP.shape[0]), size=self.POP.shape[0], replace=True, p=fitness/np.sum(fitness))]
+  
   # DNA交叉
   def crossover(self):
-    pass
+    for people in self.POP:
+      if np.random.rand() < self.cross_rate:
+        _i = np.random.randint(0, self.POP.shape[0], size=1)
+        cross_points = np.random.randint(0, 2, size=(len(self.var_len), self.DNA_SIZE)).astype(np.bool)
+        people[cross_points] = self.POP[_i, cross_points]
+
   # 基因变异
   def mutate(self):
-    pass
+    for people in self.POP:
+      for DNA in people:
+        for point in range(self.DNA_SIZE):
+          if np.random.rand() < self.mutation:
+            DNA[point] = 1 if DNA[point] == 0 else 1
+
   # 进化
   def evolution(self):
-    pass
+    self.select()
+    self.crossover()
+    self.mutate()
+  
   # 重置
   def reset(self):
-    pass
+    self.POP = self.copy_POP
+  
   # 打印当前状态日志
   def log(self):
-    pass
+    return pd.DataFrame(np.hstack((self.translateDNA(), self.get_fitness().reshape((len(self.POP),1)))), 
+                        columns=[f'x{i}' for i in range(len(self.var_len))]+['F'])
+
   # 一维变量作图
   def plot1D(self, iter_time=200):
-    pass
+    plt.ion()
+    fig = plt.figure()
+    for _ in range(iter_time):
+      plt.cla()
+      x = np.linspace(*self.bound[0], self.var_len[0]*50)
+      plt.plot(x, self.func(x))
+      x = self.translateDNA().reshape(self.POP_SIZE)
+      plt.scatter(x, self.func(x), s=200, lw=0, c='red', alpha=0.5)
+      plt.pause(0.05)
+      self.evolution()
+    
+    plt.ioff()
+
+if __name__ == '__main__':
+  func = lambda x:np.sin(10*x)*x + np.cos(2*x)*x
+  ga = GA([[np.random.rand()*5] for _ in range(100)], [(0,5)], DNA_SIZE=10, func=func)
+  ga.plot1D(50)
